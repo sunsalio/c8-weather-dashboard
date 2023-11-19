60fef50067f4eaa76c97e440f42cfb908671be9d
Salio <alio.s@outlook.com>
2023-11-19 19:03:36 +0000

    adds css, js and images folder and header element

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//  add background colour, font colour, centre to heading 
+var headerEl = $("header");
+
+headerEl.css({ "background-color": "#0C629B ", "color": "#FEF9CE", "text-align": "center", "borderRadius": "25px", "padding": "0.5px"});
+
+
+
+// use day js to add current date 
+
+var currentDate
+     // display users current city choice temp, wind, humidity
+
+// add 5 day forecast from openweather api
+
+//  store users search history to local storgae and
